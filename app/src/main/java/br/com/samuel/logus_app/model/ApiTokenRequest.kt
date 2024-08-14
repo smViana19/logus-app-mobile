448d00faa1e3f20950e21839f912e2e995e1a3da
Samuu <samuelfilipevianadois@gmail.com>
2024-08-13 22:53:38 -0300

    -adicionado anotação de SerializedName na res e req da api de token

--- app/src/main/java/br/com/samuel/logus_app/model/ApiTokenRequest.kt
+++ app/src/main/java/br/com/samuel/logus_app/model/ApiTokenRequest.kt
@@ -1,6 +1,10 @@
 package br.com.samuel.logus_app.model
 
+import com.google.gson.annotations.SerializedName
+
 data class ApiTokenRequest(
+    @SerializedName("email")
     val email: String,
+    @SerializedName("password")
     val password: String,
 )
--- app/src/main/java/br/com/samuel/logus_app/model/ApiTokenResponse.kt
+++ app/src/main/java/br/com/samuel/logus_app/model/ApiTokenResponse.kt
@@ -1,8 +1,14 @@
 package br.com.samuel.logus_app.model
 
+import com.google.gson.annotations.SerializedName
+
 data class ApiTokenResponse(
+    @SerializedName("token")
     val token: String,
+    @SerializedName("user")
     val user: ApiUserResponse,
+    @SerializedName("errors")
     val errors: List<String>,
+    @SerializedName("status")
     val status: Int
 )
