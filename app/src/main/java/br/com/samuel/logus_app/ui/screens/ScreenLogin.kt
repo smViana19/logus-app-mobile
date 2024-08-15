c1212bd214a3b87f49e8d30dcda1b5b6453f4779
Samuu <samuelfilipevianadois@gmail.com>
2024-08-15 17:42:57 -0300

    - adicionado e corrigido a logica de login, validação e dialogs dos respectivos erros

--- app/src/main/java/br/com/samuel/logus_app/ui/screens/ScreenLogin.kt
+++ app/src/main/java/br/com/samuel/logus_app/ui/screens/ScreenLogin.kt
@@ -2,6 +2,7 @@ package br.com.samuel.logus_app.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,8 +14,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,6 +32,7 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.withAnnotation
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,6 +40,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import br.com.samuel.logus_app.R
+import br.com.samuel.logus_app.ui.components.AppLoading
+import br.com.samuel.logus_app.ui.dialogs.CustomDialog
 
 
 @OptIn(ExperimentalTextApi::class)
@@ -43,115 +49,110 @@ import br.com.samuel.logus_app.R
 fun ScreenLogin(
     vm: ScreenLoginVm = hiltViewModel()
 ) {
-    var text by remember { mutableStateOf("") }
 
-    val textAnnoted = buildAnnotatedString {
-        append("Não possui uma conta?")
-        withAnnotation("squiggles", annotation = "ignored") {
-            withStyle(SpanStyle(color = Color(0xFF5A1B8D))) {
-                append(" Cadastre-se")
-            }
-        }
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Row {
-            Text(
-                text = "Olá, novamente",
-                fontSize = 24.sp,
-                fontWeight = FontWeight(600)
-            )
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-        ) {
-            Image(
-                modifier = Modifier.fillMaxSize(),
-                painter = painterResource(id = R.drawable.logo_logus),
-                contentDescription = "logo-logus"
-            )
-        }
+    var text by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
+                .fillMaxSize()
+                .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
         ) {
-            OutlinedTextField(
-                value = vm.email.value,
-                onValueChange = {newValue ->
-                    vm.email.value  = newValue
-                },
-                modifier = Modifier
-                    .fillMaxWidth(),
-                label = { Text(text = "E-mail") },
-                shape = RoundedCornerShape(10.dp),
-                trailingIcon = {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_email_24dp),
-                        contentDescription = "Email"
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.height(16.dp))
 
-            OutlinedTextField(
-                value = vm.password.value,
-                onValueChange = {newValue ->
-                    vm.password.value = newValue
-                },
-                modifier = Modifier
-                    .fillMaxWidth(),
-                label = { Text(text = "Senha") },
-                visualTransformation = PasswordVisualTransformation(),
-                shape = RoundedCornerShape(10.dp),
-                trailingIcon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_password_enabled_24dp),
-                        contentDescription = "Hide/show pass"
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                onClick = { vm.onClickLoginButton() },
-                shape = RoundedCornerShape(8.dp),
-                colors = ButtonColors(
-                    containerColor = Color(0xFF820AD1),
-                    contentColor = Color(0xFFFFFFFF),
-                    disabledContainerColor = Color(0xFF665287),
-                    disabledContentColor = Color(0xFFFFFFFF)
-                )
-            ) {
-                Text(text = "Login")
-            }
-
-            Text(
-                text = "Esqueceu sua senha?",
-                modifier = Modifier.padding(top = 16.dp),
-                color =  Color(0xFF5A1B8D)
-            )
             Row(
                 modifier = Modifier
-                    .padding(top = 16.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
+                    .fillMaxWidth()
+                    .weight(1f)
             ) {
-                Text(text = textAnnoted)
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    painter = painterResource(id = R.drawable.logo_logus),
+                    contentDescription = "logo-logus"
+                )
             }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                OutlinedTextField(
+                    value = vm.email.value,
+                    onValueChange = { newValue ->
+                        vm.email.value = newValue
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    label = { Text(text = "E-mail") },
+                    shape = RoundedCornerShape(10.dp),
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_email_24dp),
+                            contentDescription = "Email"
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                OutlinedTextField(
+                    value = vm.password.value,
+                    onValueChange = { newValue ->
+                        vm.password.value = newValue
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    label = { Text(text = "Senha") },
+                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    shape = RoundedCornerShape(10.dp),
+                    trailingIcon = {
+                        val image = if (passwordVisible)
+                            R.drawable.ic_password_disable_24dp
+                        else R.drawable.ic_password_enabled_24dp
+                        val description = if (passwordVisible) "Hide password" else "Show password"
+
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(
+                                painter = painterResource(id = image),
+                                contentDescription = description
+                            )
+                        }
+                    },
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    onClick = { vm.onClickLoginButton() },
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonColors(
+                        containerColor = Color(0xFF820AD1),
+                        contentColor = Color(0xFFFFFFFF),
+                        disabledContainerColor = Color(0xFF665287),
+                        disabledContentColor = Color(0xFFFFFFFF)
+                    )
+                ) {
+                    Text(text = "Login")
+                }
+
+                Text(
+                    text = "Esqueceu sua senha?",
+                    modifier = Modifier.padding(top = 16.dp),
+                    color = Color(0xFF5A1B8D)
+                )
+
+            }
+
+
         }
-
-
+        if (vm.isLoading.value) {
+            AppLoading()
+        }
+        CustomDialog(state = vm.dialogState.value)
     }
+
 }
 
 
--- app/src/main/java/br/com/samuel/logus_app/ui/screens/ScreenLoginVm.kt
+++ app/src/main/java/br/com/samuel/logus_app/ui/screens/ScreenLoginVm.kt
@@ -1,11 +1,15 @@
 package br.com.samuel.logus_app.ui.screens
 
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.samuel.logus_app.backendservices.api.ApiService
+import br.com.samuel.logus_app.backendservices.sharedprefs.SharedPrefsService
 import br.com.samuel.logus_app.model.ApiTokenRequest
+import br.com.samuel.logus_app.ui.dialogs.DialogState
+import br.com.samuel.logus_app.ui.dialogs.DialogType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
@@ -13,34 +17,86 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ScreenLoginVm @Inject constructor(
-    private val apiService: ApiService
+    private val apiService: ApiService,
+    private val sharedPrefsService: SharedPrefsService
 ) : ViewModel() {
     private val _email = mutableStateOf("")
     val email: MutableState<String> = _email
     private val _password = mutableStateOf("")
     val password: MutableState<String> = _password
 
+    private val _isLoading = mutableStateOf(false)
+    val isLoading: MutableState<Boolean> = _isLoading
+
+    private val _dialogState = mutableStateOf(DialogState(open = false))
+    val dialogState: State<DialogState> = _dialogState
+
+    private fun saveUser(name: String, email: String, password: String, role: String) {
+        sharedPrefsService.saveUserData(name, email, password, role)
+    }
 
     fun onClickLoginButton() {
         viewModelScope.launch {
 
             val apiTokenRequest = ApiTokenRequest(email = _email.value, password = _password.value)
             try {
+                _isLoading.value = true
                 val response = apiService.requestTokenLogin(apiTokenRequest)
                 if(response.status != 200) {
                     val responseErrors = response.errors
                     println("errors: $responseErrors")
+                    _isLoading.value = false
+                    _dialogState.value = DialogState(
+                        open = true,
+                        type = DialogType.ERROR,
+                        title = "Erro ao fazer login",
+                        msg = responseErrors.toString()
+                    )
                 } else {
                     val token = response.token
+                    val role = response.user.role
+                    val name = response.user.name
                     println("login successfully, Token: $token")
+                    saveUser(name, email.value, password.value, role)
+                    println("saveuser:$name, $role ")
+                    _isLoading.value = false
+                    _dialogState.value = DialogState(
+                        open = true,
+                        type = DialogType.SUCCESS,
+                        title = "Sucesso",
+                        msg = response.msgSuccess,
+                        onConfirm = {
+                            onClickButtonDismissDialog()
+                        }
+                    )
                 }
             } catch (e: HttpException) {
                 val errorBody = e.response()?.errorBody()?.string()
-                println("status: ${e.code()} - ${errorBody ?: "Unknown error"}")
-            } catch (t: Throwable) {
-                t.printStackTrace()
+                val errorMessage = errorBody?.substringAfterLast("\"errors\":[\"")?.substringBeforeLast("\"]") ?: "Erro desconhecido"
+                println(errorBody)
+                _isLoading.value = false
+                _dialogState.value = DialogState(
+                    open = true,
+                    type = DialogType.ALERT,
+                    title = "Erro ao fazer login",
+                    msg = errorMessage,
+                    onConfirm = {
+                        onClickButtonDismissDialog()
+                    }
+                )
+
+            } finally {
+                _isLoading.value = false
             }
         }
+    }
+
+
+
+    private fun onClickButtonDismissDialog() {
+        _dialogState.value = DialogState(
+            open = false
+        )
     }
 
 }
