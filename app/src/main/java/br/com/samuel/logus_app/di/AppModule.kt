306ef3f675ac3754fdfd74434cc690f8bfcd2fea
Samuu <samuelfilipevianadois@gmail.com>
2024-08-15 17:38:08 -0300

    -module para a aplicação inteira depencency injector

--- app/src/main/java/br/com/samuel/logus_app/di/AppModule.kt
+++ app/src/main/java/br/com/samuel/logus_app/di/AppModule.kt
@@ -1,20 +1,22 @@
 package br.com.samuel.logus_app.di
 
+import android.content.Context
 import br.com.samuel.logus_app.backendservices.api.ApiService
+import br.com.samuel.logus_app.backendservices.sharedprefs.SharedPrefsService
+import br.com.samuel.logus_app.backendservices.sharedprefs.SharedPrefsServiceImpl
 import br.com.samuel.logus_app.constants.APP_URL
-import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-
 @Module
 @InstallIn(SingletonComponent::class)
-object RetrofitModule {
+object AppModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
@@ -36,5 +38,8 @@ object RetrofitModule {
         return retrofit.create(ApiService::class.java)
     }
 
-
+    @Provides
+    fun provideSharedPrefsService(@ApplicationContext context: Context): SharedPrefsService {
+        return SharedPrefsServiceImpl(context)
+    }
 }
