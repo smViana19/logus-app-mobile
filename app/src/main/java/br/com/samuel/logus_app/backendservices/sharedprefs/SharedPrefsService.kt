e31125d96c9b8e7fb7745a3ff6da068b6b8f5e57
Samuu <samuelfilipevianadois@gmail.com>
2024-08-23 21:57:24 -0300

    Documentação dos mapeamentos do SharedPrefs -Adicionado kdoc

--- app/src/main/java/br/com/samuel/logus_app/backendservices/sharedprefs/SharedPrefsService.kt
+++ app/src/main/java/br/com/samuel/logus_app/backendservices/sharedprefs/SharedPrefsService.kt
@@ -2,9 +2,26 @@ package br.com.samuel.logus_app.backendservices.sharedprefs
 
 import br.com.samuel.logus_app.model.ApiUserRequest
 
-interface SharedPrefsService {
 
+/**
+ * An interface that defines the shared preferences service.
+ */
+
+interface SharedPrefsService {
+    /**
+     * Saves user datato shared preferences.
+     *
+     * @param name The user's name.
+     * @param email The user's email.
+     * @param password The user's password.
+     * @param role The user's role.
+     */
     fun saveUserData(name: String, email: String, password: String, role: String)
+    /**
+     * Gets user data from shared preferences.
+     *
+     * @return The user data as an ApiUserRequest object.
+     */
     fun getUserData(): ApiUserRequest
 
 }
